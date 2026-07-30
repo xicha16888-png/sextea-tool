73b30f6f95b849e49caddc50cf11e064b3d33e90
fancyfx <xicha16888@gmail.com>
2026-07-30 09:39:30 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Replicate = require('replicate');
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname));
+
+const replicate = new Replicate({
+  auth: 'r8_UQWY369ejx1TYyXB3SCsFCdnT5cMWuH1lOtDY'
+});
+
+app.post('/api/generate', async (req, res) => {
+  try {
+    const output = await replicate.run(
+      'black-forest-labs/flux-schnell',
+      { input: req.body.input }
+    );
+    res.json({ status: 'succeeded', output });
+  } catch(e) {
+    res.status(500).json({ detail: e.message });
+  }
+});
+
+app.listen(3099, () => console.log('OK: http://localhost:3099'));
